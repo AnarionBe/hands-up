bbe95d13b83e61e43dffbfbca447125ca12323c7
AnarionBe <marcodb.debona@gmail.com>
2021-01-17 20:12:12 +0100

    gh-page

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,59 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
-module.exports = {
-	entry: ['./src/index.js'],
-	output: {
-			path: __dirname + '/build',
-			publicPath: '/',
-			filename: 'bundle.js'
-	},
-	devtool: 'source-map',
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader"
-				}
-			},
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: "html-loader"
+module.exports = env => {
+	const entry = env === 'production' ? './' : '/';
+
+	return {
+		entry: ['./src/index.js'],
+		output: {
+				path: __dirname + '/build',
+				publicPath: entry,
+				filename: 'bundle.js'
+		},
+		devtool: 'source-map',
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader"
 					}
-				]
-			},
-			{
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
-			},
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./src/index.html",
-			filename: "index.html"
-		})
-	],
-	devServer: {
-		contentBase: path.join(__dirname, "./build"),
-		watchContentBase: true,
-		historyApiFallback: true,
-		inline: true,
-		open: false,
-		hot: true,
-		disableHostCheck: true,
-		port: 8888,
+				},
+				{
+					test: /\.html$/,
+					use: [
+						{
+							loader: "html-loader"
+						}
+					]
+				},
+				{
+					test: /\.s[ac]ss$/i,
+					use: [
+						'style-loader',
+						'css-loader',
+						'sass-loader',
+					],
+				},
+			]
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: "./src/index.html",
+				filename: "index.html"
+			})
+		],
+		devServer: {
+			contentBase: path.join(__dirname, "./build"),
+			watchContentBase: true,
+			historyApiFallback: true,
+			inline: true,
+			open: false,
+			hot: true,
+			disableHostCheck: true,
+			port: 8888,
+		}
 	}
 };
